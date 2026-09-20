8ed95084c2bc05669f2dbbbc015cf6db6b289212
Ben-Salem <salemko801@gmail.com>
2026-09-20 18:08:17 +0100

    Configurer le serveur pour le frontend

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,40 @@
 const express = require('express');
-// Importation des routes pour les auteurs
+const path = require('path');
+
+// Importation des routes
 const auteurRoutes = require('./routes/auteurRoutes');
 const adherentRoutes = require('./routes/adherentRoutes');
 const livreRoutes = require('./routes/livreRoutes');
 const empruntRoutes = require('./routes/empruntRoutes');
-
-const logger = require('./middlewares/logger');
-const errorHandler = require('./middlewares/errorHandler');
-const notFound = require('./middlewares/notFound');
-
 const statistiqueRoutes = require('./routes/statistiqueRoutes');
+
+// Importation des middlewares
+const logger = require('./middlewares/logger');
+const notFound = require('./middlewares/notFound');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
+// Permet à Express de servir le frontend
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Middleware de journalisation
 app.use(logger);
-app.use(errorHandler);
-// Utilisation des routes pour les auteurs
+
+// Routes API
 app.use('/api/auteurs', auteurRoutes);
 app.use('/api/adherents', adherentRoutes);
 app.use('/api/livres', livreRoutes);
 app.use('/api/emprunts', empruntRoutes);
-app.use('/api/statistiques', statistiqueRoutes);    
+app.use('/api/statistiques', statistiqueRoutes);
 
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'API Bibliothèque opérationnelle'
-  });
-});
-
+// Route introuvable
 app.use(notFound);
+
+// Gestionnaire d'erreurs
 app.use(errorHandler);
 
 app.listen(PORT, () => {
